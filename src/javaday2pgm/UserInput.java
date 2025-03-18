3e20ff3b55c49acd712db5c2dc1919da3a7733a0
Harshini0421 <harshini.veerabathiran04@gmail.com>
2025-03-18 14:06:13 +0530

    Add files via upload

---
+++ src/javaday2pgm/UserInput.java
@@ -0,0 +1,214 @@
+package javaday2pgm;
+
+import java.util.*;
+
+
+
+public class UserInput {
+
+	public static void main(String [] args) {
+
+		
+
+		
+
+			 //	String name = "ram";
+
+				String name = new String("ram");
+
+				System.out.println(name);
+
+		
+
+			
+
+
+
+		
+
+
+
+		
+
+		
+
+		/*Scanner s1 = new Scanner(System.in);
+
+		System.out.println("ENTER A NAME: ");
+
+		String name = s1.nextLine();
+
+		System.out.println(name);
+
+	*/
+
+		
+
+		// string and substring and indexof
+
+		//String str ="Javz programming";
+
+	/*	System.out.println(str.length()); // starting index
+
+		System.out.println(str.substring(3)); //
+
+		System.out.println(str.substring(0,4)); 
+
+		System.out.println(str.indexOf("prog"));
+
+		System.out.println(str.indexOf("x")); */
+
+		
+
+		
+
+		/*System.out.println(str.toUpperCase());
+
+		System.out.println(str.toLowerCase());*/
+
+		
+
+		
+
+		//System.out.println(str.replace('a','A'));
+
+		
+
+		/*String str = "python";
+
+		String str1 = "loop";
+
+		System.out.println(str==str1);
+
+		System.out.println(str.equals(str1));
+
+		
+
+		System.out.println(str1.charAt(2));*/
+
+		
+
+		//split 
+
+		/*String str ="java,py,c++,ruby";
+
+		String[] languange = str.split(",");
+
+		for(String s1:languange) {
+
+			System.out.println(s1);
+
+			
+
+		}*/
+
+		
+
+		//concadenate
+
+		/*String str ="java";
+
+		String str1="programming";
+
+		System.out.println("concat string: "+str.concat(str1));//concadanate */
+
+		
+
+		/*String a1=new String("JavaDeveloper");
+
+
+
+		  String b1="JavaDeveloper";
+
+
+
+		  String c1=a1.intern();
+
+
+
+		  System.out.println(a1==b1);
+
+
+
+		  System.out.println(b1==c1);*/
+
+		
+
+		
+
+		/*String str ="java";
+
+		String str1 = "python";
+
+		System.out.println(str.compareTo(str1));*/
+
+		
+
+		
+
+		//string seq
+
+	/*	String str ="java programming";
+
+		System.out.println(str.contains("java"));
+
+		System.out.println(str.contains("python")); */
+
+		
+
+		
+
+		/*String s1 = new String("java developer");
+
+		String s2 = new String ("java developer");
+
+		String s3 = s1.intern();
+
+		System.out.println()*/
+
+		
+
+
+
+	/*	 StringBuilder sb = new StringBuilder("hello");
+
+		//append
+
+		sb.append("world");
+
+		System.out.println("after append :"+sb);
+
+		sb.insert(5,", ");
+
+		System.out.println("after insert :"+sb); 
+
+		//reverse
+
+		sb.reverse();
+
+		System.out.println("after reverse :"+sb); 
+
+		 */
+
+		
+
+		
+
+		/* //string buffer
+
+		StringBuffer sb = new StringBuffer("java");
+
+		sb.append("programming");
+
+		System.out.println("stringbuffer:"+sb); */
+
+		
+
+		
+
+	
+
+	}
+
+}
+
